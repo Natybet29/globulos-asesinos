4961c3685330a5d0b935407b2528661fcaac6dad
Natybet29 <92266341+Natybet29@users.noreply.github.com>
2021-11-06 15:41:47 -0600

    repositoriojuego

---
+++ Assets/Playercontroller.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    bool chasingPlayer = true;
+    public Transform player;
+    public speed = 1;
+    public float pushForce = 1;
+    Rigidbody rig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     rig = GetComponent<Rigidbody>()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+      if (chasingPlayer)
+      {
+          transform. LookAt(player.position, ); 
+          rig.velocity = transform.forward * speed + new Vector3(0, 0, rig.velocity.z)  
+    }
+}
+private void OnTriggerEnter(Collider other)
+{
+    if(other.gameObejct.tag == "Player")
+    {
+        Rigidbody PlayerRig = collision.gameObject.GetComponent<Rigidbody>();
+        playerRig.velocity = transform.forward * pushdorce * new Vector3(0, 0, player.velocity.z)
+        chasigPlayer = true;
+    }
+}
+
+private void OnTriggerEnter(Collider other)
+{
+    if(other.gameObejct.tag == "Player")
+    {
+        chasigPlayer = false;
+    }
+}
+   
+   void OnCollisionEnter(Collision collision)
+   
+   {
+       if (collision.gameObject.tag == "Player")
+       {
+           Destroy(collicion.gameObejct.tag == "Player")
+           
+       }
+       
+       audioSource.Play();
+    }
